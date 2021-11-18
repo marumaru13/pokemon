2ef6c1f90416a852b5725fd791a10f0f89b8ae1d
mai yamamaru <for.life0311@gmail.com>
2021-11-18 16:37:08 +0900

    jsにajax部分追加

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,5 +9,17 @@ $(function(){
         cache: false,        //cacheを使うか使わないかを設定
         dataType:'json',     //data type script・xmlDocument・jsonなど
     })
+    .done(function(response) { 
+        //通信成功時の処理
+        //成功したとき実行したいスクリプトを記載
+     })
+     .fail(function(xhr) {  
+　　　　　 //通信失敗時の処理
+        //失敗したときに実行したいスクリプトを記載
+     })
+     .always(function(xhr, msg) { 
+　　　　　//通信完了時の処理
+       //結果に関わらず実行したいスクリプトを記載
+  });   
 
 });
